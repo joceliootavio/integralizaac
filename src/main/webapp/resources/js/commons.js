5f41503f43ee2deab6f8eeb8952e28a6afae557a
jocelio <jocelio@192.168.0.11>
2016-11-09 22:24:52 -0200

    permitir somente números na matrícula do aluno

--- src/main/webapp/resources/js/commons.js
+++ src/main/webapp/resources/js/commons.js
@@ -18,8 +18,7 @@ function hideStatusDialog() {
 function validateNumber(event) {
     var key = window.event ? event.keyCode : event.which;
 
-    if (event.keyCode == 8 || event.keyCode == 46
-     || event.keyCode == 37 || event.keyCode == 39) {
+    if (event.keyCode == 8) {
         return true;
     }
     else if ( key < 48 || key > 57 ) {
